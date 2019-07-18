97cc801138cfaef528b5ac7b9c7533a63a9b4b05
Ike Uchendu <uchendui@msu.edu>
2019-07-18 15:59:02 -0700

    Finished reinforce without baseline

--- REINFORCE/reinforce.py
+++ REINFORCE/reinforce.py
@@ -1,7 +1,20 @@
+import gym
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder
 from util.network import ReinforceNetworkBuilder
+
+
+class Trajectory:
+    def __init__(self, states, actions, rewards, num_actions):
+        self.states = np.array(states)
+        actions = np.array(actions).reshape(-1, 1)
+        oh = OneHotEncoder(sparse=False, categories='auto')
+        self.actions = oh.fit_transform(actions)
+        self.q_values = np.zeros(shape=len(self.actions))
+        for i in range(len(states)):
+            self.q_values[i] = np.sum(rewards[i:])
 
 
 class TrainReinforce:
@@ -15,8 +28,9 @@ class TrainReinforce:
                  save_path=None,
                  ):
         self.max_episodes = max_episodes
+        self.print_freq = print_freq
         self.env = env
-        self.input_dim = env.observation_space.shape
+        self.input_dim = env.observation_space.shape[0]
         self.output_dim = env.action_space.n
         self.sess = sess
         self.render = render
@@ -26,31 +40,68 @@ class TrainReinforce:
                                            activations=(tf.nn.relu, tf.nn.softmax),
                                            scope='reinforce_network')
 
+    def act(self, observation):
+        pred = self.sess.run(self.rnb.output_pred,
+                             feed_dict={self.rnb.input_ph: np.reshape(observation, (1, self.input_dim))})
+        return np.random.choice(range(self.output_dim), p=pred.flatten())
+
     def reinforce(self):
+        avg_reward = 0
         for e in range(self.max_episodes):
             # Sample trajectories from our policy (run it on the robot)
-            actions, states, q_values = self.sample()
+            traj, reward = self.sample()
+            avg_reward += reward
 
             # Compute and apply the gradient of the log policy multiplied by our baseline
-            self.update(states, actions, q_values)
+            self.update(traj.states, traj.actions, traj.q_values)
+
+            if e % self.print_freq == 0 and e > 0:
+                new_mean_reward = avg_reward / self.print_freq
+                avg_reward = 0
+                print(f"-------------------------------------------------------")
+                print(f"Mean {self.print_freq} Episode Reward: {new_mean_reward}")
+                # print(f"Exploration fraction: {eps}")
+                print(f"Total Episodes: {e}")
+                # print(f"Total timesteps: {t}")
+                print(f"-------------------------------------------------------")
 
     def update(self, states, actions, q_values):
-        pass
+        self.sess.run([self.rnb.opt, self.rnb.loss], feed_dict={self.rnb.input_ph: states,
+                                                                self.rnb.actions_ph: actions,
+                                                                self.rnb.q_values_ph: q_values})
 
     def sample(self):
         done = False
-        obs = self.env.reset()
         actions = []
         states = []
+        rewards = []
 
+        obs = self.env.reset()
+        total_reward = 0
+        ep_len = 0
         while not done:
+            states.append(obs)
             if self.render:
                 self.env.render()
-            obs, rew, done, _ = self.env.step()
+
+            action = self.act(obs)
+            obs, rew, done, _ = self.env.step(action)
+
+            total_reward += rew
+            ep_len += 1
+
+            rewards.append(rew)
+            actions.append(action)
+        return Trajectory(states, actions, rewards, self.output_dim), total_reward
 
 
 def main():
-    pass
+    with tf.Session() as sess:
+        env_name = 'CartPole-v0'
+        env = gym.make(env_name)
+        reinforce = TrainReinforce(env, sess, render=False, max_episodes=2000, print_freq=10)
+        sess.run(tf.initialize_all_variables())
+        reinforce.reinforce()
 
 
 if __name__ == '__main__':
--- util/network.py
+++ util/network.py
@@ -3,7 +3,7 @@ from tensorflow.contrib import layers
 
 
 class NetworkBuilder:
-    def __init__(self, input_dim, output_dim, layers=(512,), activations=(tf.nn.relu, None), scope='q_network',
+    def __init__(self, input_dim, output_dim, layers=(512,), activations=(tf.nn.relu, None),
                  conv=False):
         assert len(layers) > 0, 'There must be at least one hidden layer'
 
@@ -12,97 +12,95 @@ class NetworkBuilder:
         self.layer_sizes = layers
         self.activations = activations
         self.conv = conv
-        self.scope = scope
 
     def create_network(self):
-        with tf.variable_scope(self.scope):
-            if self.conv:
-                input_ph = tf.placeholder(dtype=tf.float32, shape=[None, *self.input_dim])
-                network = tf.image.rgb_to_grayscale(input_ph)
-                network = layers.conv2d(network, 30, kernel_size=4, activation_fn=tf.nn.relu, stride=4)
-                network = layers.conv2d(network, 30, kernel_size=2, activation_fn=tf.nn.relu, stride=2)
-                network = layers.conv2d(network, 30, kernel_size=1, activation_fn=tf.nn.relu, stride=1)
-                network = layers.flatten(network)
-                network = layers.fully_connected(network, 512, activation_fn=tf.nn.relu)
-                output_pred = layers.fully_connected(network, self.output_dim, activation_fn=None)
+        if self.conv:
+            input_ph = tf.placeholder(dtype=tf.float32, shape=[None, *self.input_dim])
+            network = tf.image.rgb_to_grayscale(input_ph)
+            network = layers.conv2d(network, 30, kernel_size=4, activation_fn=tf.nn.relu, stride=4)
+            network = layers.conv2d(network, 30, kernel_size=2, activation_fn=tf.nn.relu, stride=2)
+            network = layers.conv2d(network, 30, kernel_size=1, activation_fn=tf.nn.relu, stride=1)
+            network = layers.flatten(network)
+            network = layers.fully_connected(network, 512, activation_fn=tf.nn.relu)
+            output_pred = layers.fully_connected(network, self.output_dim, activation_fn=None)
 
-            else:
-                weights = [tf.get_variable(name='W0',
-                                           shape=[self.input_dim, self.layer_sizes[0]],
-                                           initializer=tf.contrib.layers.xavier_initializer())]
-                biases = [tf.get_variable(name=f'b0',
-                                          shape=[self.layer_sizes[0]],
-                                          initializer=tf.constant_initializer(0.))]
-                for i in range(1, len(self.layer_sizes)):
-                    in_dim = weights[-1].shape[1].value
-                    out_dim = self.layer_sizes[i]
-                    weights.append(tf.get_variable(name=f'W{i}',
-                                                   shape=[in_dim, out_dim],
-                                                   initializer=tf.contrib.layers.xavier_initializer()))
-                    biases.append(tf.get_variable(name=f'b{i}',
-                                                  shape=[out_dim],
-                                                  initializer=tf.constant_initializer(0.)))
-
-                weights.append(tf.get_variable(name=f'W{len(self.layer_sizes)}',
-                                               shape=[self.layer_sizes[-1], self.output_dim],
+        else:
+            weights = [tf.get_variable(name='W0',
+                                       shape=[self.input_dim, self.layer_sizes[0]],
+                                       initializer=tf.contrib.layers.xavier_initializer())]
+            biases = [tf.get_variable(name=f'b0',
+                                      shape=[self.layer_sizes[0]],
+                                      initializer=tf.constant_initializer(0.))]
+            for i in range(1, len(self.layer_sizes)):
+                in_dim = weights[-1].shape[1].value
+                out_dim = self.layer_sizes[i]
+                weights.append(tf.get_variable(name=f'W{i}',
+                                               shape=[in_dim, out_dim],
                                                initializer=tf.contrib.layers.xavier_initializer()))
-                biases.append(tf.get_variable(name=f'b{len(self.layer_sizes)}',
-                                              shape=[self.output_dim],
+                biases.append(tf.get_variable(name=f'b{i}',
+                                              shape=[out_dim],
                                               initializer=tf.constant_initializer(0.)))
 
-                # Create computation graph for network
-                input_ph = tf.placeholder(dtype=tf.float32, shape=[None, self.input_dim])
-                layer = input_ph
-                for W, b, activation in zip(weights, biases, self.activations):
-                    layer = tf.nn.xw_plus_b(x=layer, weights=W, biases=b)
-                    if activation is not None:
-                        layer = activation(layer)
-                output_pred = layer
+            weights.append(tf.get_variable(name=f'W{len(self.layer_sizes)}',
+                                           shape=[self.layer_sizes[-1], self.output_dim],
+                                           initializer=tf.contrib.layers.xavier_initializer()))
+            biases.append(tf.get_variable(name=f'b{len(self.layer_sizes)}',
+                                          shape=[self.output_dim],
+                                          initializer=tf.constant_initializer(0.)))
 
-            return input_ph, output_pred
+            # Create computation graph for network
+            input_ph = tf.placeholder(dtype=tf.float32, shape=[None, self.input_dim])
+            layer = input_ph
+            for W, b, activation in zip(weights, biases, self.activations):
+                layer = tf.nn.xw_plus_b(x=layer, weights=W, biases=b)
+                if activation is not None:
+                    layer = activation(layer)
+            output_pred = layer
+
+        return input_ph, output_pred
 
 
 class QNetworkBuilder(NetworkBuilder):
     def __init__(self, input_dim, output_dim, layers=(512,), activations=(tf.nn.relu, None), scope='q_network',
                  conv=False):
-        super(QNetworkBuilder, self).__init__(input_dim, output_dim, layers, activations, scope, conv)
+        super(QNetworkBuilder, self).__init__(input_dim, output_dim, layers, activations, conv)
+        with tf.variable_scope(scope):
+            # Create variables
+            self.input_ph, self.output_pred = self.create_network()
+            self.target_ph = tf.placeholder(dtype=tf.float32, shape=[None])
+            self.action_indices_ph = tf.placeholder(dtype=tf.int32, shape=[None])
+            self.saver = tf.train.Saver()
 
-        # Create variables
-        self.input_ph, self.output_pred = self.create_network()
-        self.target_ph = tf.placeholder(dtype=tf.float32, shape=[None])
-        self.action_indices_ph = tf.placeholder(dtype=tf.int32, shape=[None])
-        self.saver = tf.train.Saver()
+            # Create loss function
+            batch_range = tf.range(start=0, limit=tf.shape(self.action_indices_ph)[0])
+            indices = tf.stack((batch_range, self.action_indices_ph), axis=1)
+            self.action_values = tf.gather_nd(self.output_pred, indices)
+            self.loss = tf.losses.mean_squared_error(self.target_ph, self.action_values)
 
-        # Create loss function
-        batch_range = tf.range(start=0, limit=tf.shape(self.action_indices_ph)[0])
-        indices = tf.stack((batch_range, self.action_indices_ph), axis=1)
-        self.action_values = tf.gather_nd(self.output_pred, indices)
-        self.loss = tf.losses.mean_squared_error(self.target_ph, self.action_values)
-
-        # Optimizer
-        self.adam = tf.train.AdamOptimizer()
-        self.opt = tf.train.AdamOptimizer().minimize(self.loss)
+            # Optimizer
+            self.adam = tf.train.AdamOptimizer()
+            self.opt = tf.train.AdamOptimizer().minimize(self.loss)
 
 
 class ReinforceNetworkBuilder(NetworkBuilder):
     def __init__(self, input_dim, output_dim, layers=(512,), activations=(tf.nn.relu, None), scope='q_network',
                  conv=False):
-        super(ReinforceNetworkBuilder, self).__init__(input_dim, output_dim, layers, activations, scope, conv)
+        super(ReinforceNetworkBuilder, self).__init__(input_dim, output_dim, layers, activations, conv)
+        with tf.variable_scope(scope):
+            # Create additional variables
+            self.input_ph, self.output_pred = self.create_network()
+            self.q_values_ph = tf.placeholder(dtype=tf.float32, shape=[None], name='Reward-to-go')
+            self.actions_ph = tf.placeholder(dtype=tf.float32, shape=[None, self.output_dim], name='actions')
 
-        # Create variables
-        self.input_ph, self.output_pred = self.create_network()
-        self.q_values_ph = tf.placeholder(dtype=tf.float32, shape=[None])
-        self.actions_ph = tf.placeholder(dtype=tf.float32, shape=[None, self.output_dim])
+            # Create loss function for policy gradient
+            logits = self.output_pred
+            negative_likelihoods = tf.nn.softmax_cross_entropy_with_logits(labels=self.actions_ph, logits=logits)
+            weighted_negative_likelihoods = tf.multiply(negative_likelihoods, self.q_values_ph)
+            self.loss = tf.reduce_mean(weighted_negative_likelihoods)
 
-        # Create loss function for policy gradient
-        logits = self.output_pred
-        negative_likelihoods = tf.nn.softmax_cross_entropy_with_logits(labels=self.actions_ph, logits=logits)
-        weighted_negative_likelihoods = tf.multiply(negative_likelihoods, self.q_values_ph)
-        self.loss = tf.reduce_mean(weighted_negative_likelihoods)
-
-        # Optimizer
-        self.adam = tf.train.AdamOptimizer()
-        self.opt = tf.train.AdamOptimizer().minimize(self.loss)
+            # Optimizer
+            self.adam = tf.train.AdamOptimizer()
+            self.opt = tf.train.AdamOptimizer().minimize(self.loss)
 
 
 def main():
