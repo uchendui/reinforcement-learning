cfb82c2d1cd90b241dc48bb048b1152cfbe9acba
Ike Uchendu <uchendui@msu.edu>
2019-07-22 15:26:10 -0700

    Working tensorflow distributed server

--- A2C/a2c.py
+++ A2C/a2c.py
@@ -1,10 +1,12 @@
+import os
 import gym
 import sys
 import time
+import json
 import logging
 import numpy as np
-import multiprocessing
 import tensorflow as tf
+import multiprocessing as mp
 
 from absl import flags
 from util.ports import find_open_ports
@@ -16,64 +18,75 @@ MSG_STEP = 'step'
 MSG_RESET = 'reset'
 
 
-class Worker(multiprocessing.Process):
-    def __init__(self, worker_id, conn, env_name,
-                 cluster_config,
-                 # server,
-                 input_dim, output_dim, render):
+class Worker(mp.Process):
+    def __init__(self, worker_id,
+                 conn,
+                 experiences,
+                 env_name,
+                 input_dim,
+                 output_dim,
+                 render):
         super(Worker, self).__init__()
         self.id = worker_id
-        self.conn = conn
-        self.cluster = cluster_config
         self.env = gym.make(env_name)
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.render = render
+        self.conn = conn
+        self.experiences = experiences
+        self.obs = self.env.reset()
 
-    def reset(self):
-        self.env.reset()
-        for i in range(1000):
-            if self.render:
-                self.env.render()
-            obs, rew, done, _ = self.env.step(self.env.action_space.sample())
-            time.sleep(0.01)
-            if done:
-                obs = self.env.reset()
+    def act(self, observation):
+        """Select an action according to the policy."""
+        pred = self.sess.run(self.ac.actor.output_pred,
+                             feed_dict={self.ac.actor.input_ph: np.reshape(observation, (1, self.input_dim))})
+        return np.random.choice(range(self.output_dim), p=pred.flatten())
 
-    def act(self, sess, observation):
-        return self.env.action_space.sample()
-
-    def step(self, observation):
-        action = self.act(observation)
+    def step(self):
+        """Performs a single step of the environment and adds a transition to the experience queue."""
+        if self.render:
+            self.env.render()
+        action = self.act(self.obs)
         obs, rew, done, _ = self.env.step(action)
+        self.experiences.put((self.obs, action, rew, obs, done))
+        self.obs = obs
+        if done:
+            self.obs = self.env.reset()
+
+    def listen_for_commands(self):
+        """Listens for commands from the global worker."""
+        while True:
+            msg = self.conn.recv()
+            if msg == MSG_STOP:
+                # self.server.join()
+                break
+            elif msg == MSG_RESET:
+                pass
+            elif msg == MSG_STEP:
+                self.step()
+            print(f'{msg} from worker {self.id}')
 
     def run(self):
-        server = tf.train.Server(self.cluster, job_name='worker', task_index=self.id)
-        with tf.Session(target=server.target) as sess:
-            with tf.device("/job:global/task:0"):
-                self.ac = ActorCriticNetworkBuilder(self.input_dim, self.output_dim)
+        config = json.loads(os.environ.get('TF_CONFIG'))
+        cluster = tf.train.ClusterSpec(config)
+        self.server = tf.distribute.Server(cluster, job_name='worker', task_index=self.id)
+        self.create_parameters()
+        self.listen_for_commands()
 
-            while len(sess.run(tf.report_uninitialized_variables())) > 0:
-                print("Worker %d: waiting for variable initialization..." % self.id)
-                time.sleep(1.0)
-            print("Worker %d: variables initialized" % self.id)
+    def create_parameters(self):
+        self.ac = ActorCriticNetworkBuilder(self.input_dim, self.output_dim)
+        print('Worker: Created variables')
+        self.sess = tf.Session(target=self.server.target)
+        print('Worker Session Created')
 
-            # Wait for commands to be passed
-            while True:
-                msg = self.conn.recv()
-                if msg == MSG_STOP:
-                    # self.server.join()
-                    break
-                elif msg == MSG_RESET:
-                    self.reset()
-                elif msg == MSG_STEP:
-                    self.step(observation=)
-                print(f'{msg} from worker {self.id}')
+        while len(self.sess.run(tf.report_uninitialized_variables())) > 0:
+            print("Worker %d: waiting for variable initialization..." % self.id)
+            time.sleep(1.0)
+        print("Worker %d: variables initialized" % self.id)
 
 
 class TrainA2C:
     def __init__(self, num_workers, env_name, max_steps=40000, render=False):
-        self.par_connections, self.child_connections = zip(*[multiprocessing.Pipe() for i in range(num_workers)])
         self.max_steps = max_steps
         self.render = render
         self.num_workers = num_workers
@@ -81,61 +94,74 @@ class TrainA2C:
         self.env_name = env_name
         self.input_dim = env.observation_space.shape[0]
         self.output_dim = env.action_space.n
-        with tf.device("/job:global/task:0"):
-            self.ac = ActorCriticNetworkBuilder(self.input_dim, self.output_dim)
-        self.create_parameter_server()
-
-    def create_parameter_server(self):
-        # Create parameter server: http://amid.fish/distributed-tensorflow-a-gentle-introduction
-
-        ports = find_open_ports(self.num_workers + 1)
-        jobs = {'global': [f'127.0.0.1:{ports[0]}'],
-                'worker': [f'127.0.0.1:{ports[i + 1]}' for i in range(self.num_workers)]}
-
-        self.cluster = tf.train.ClusterSpec(jobs)
-        self.server = tf.distribute.Server(self.cluster,
-                                           job_name='global',
-                                           task_index=0)
+        self.par_connections, self.child_connections = zip(*[mp.Pipe() for i in range(num_workers)])
+        self.experiences = mp.Queue()
         self.workers = [Worker(
             worker_id=i,
-            # server=worker_servers[i],
-            cluster_config=self.cluster,
             conn=self.child_connections[i],
+            experiences=self.experiences,
             env_name=self.env_name,
             render=self.render,
             input_dim=self.input_dim,
             output_dim=self.output_dim) for i in range(self.num_workers)]
+        self.create_config()
 
-    def learn(self, sess):
-        # Reset the environ
-        mean_reward = None
-        total_reward = 0
-        ep = 0
+    def create_config(self):
+        """Creates distributed tensorflow configuration"""
 
-        for t in range(self.max_steps):
-            pass
+        ports = find_open_ports(self.num_workers + 1)
+        jobs = {'global': [f'127.0.0.1:{ports[0]}'],
+                'worker': [f'127.0.0.1:{ports[i + 1]}' for i in range(self.num_workers)]}
+        os.environ['TF_CONFIG'] = json.dumps(jobs)
+
+        # config = json.loads(os.environ.get('TF_CONFIG'))
+        cluster = tf.train.ClusterSpec(jobs)
+        self.server = tf.distribute.Server(cluster, job_name='global', task_index=0)
+        self.sess = tf.Session(target=self.server.target)
+        self.ac = ActorCriticNetworkBuilder(self.input_dim, self.output_dim)
+
+    def get_transitions(self):
+        """Waits for every environment to be sampled."""
+        not_rec = self.num_workers
+        while not_rec > 0:
+            while not self.experiences.empty():
+                print(self.experiences.get())
+                not_rec -= 1
+
+    def learn(self):
+        """Trains an agent via advantage actor-critic"""
+        self.sess.run(tf.global_variables_initializer())
+        for t in range(1000):
+            # Sample from each environment
+            self.send_message(MSG_STEP)
+
+            # Aggregate transitions
+            self.get_transitions()
+        self.send_message(MSG_STOP)
 
     def send_message(self, msg):
+        """Sends a message to all of the worker processes via pipes.
+        Args:
+            msg: constant to to send to worker pipes.
+        """
         for conn in self.par_connections:
             conn.send(msg)
 
     def start(self):
-        for w in self.workers:
+        """Starts the worker processes and waits until they are done."""
+        for i, w in enumerate(self.workers):
             w.start()
-        with tf.Session(target=self.server.target) as sess:
-            sess.run(tf.global_variables_initializer())
-            self.learn(sess)
-            self.send_message(MSG_RESET)
-            self.send_message(MSG_STOP)
-
+            if i < self.num_workers - 1:
+                time.sleep(5)  # Need to separate workers starting due to resource usage
+        self.learn()
         for w in self.workers:
             w.join()
-        # sess.close()
 
 
 def main():
-    a2c = TrainA2C(2, render=False, env_name='CartPole-v0')
-    a2c.start()
+    mp.set_start_method('spawn')
+    t = TrainA2C(2, 'CartPole-v0', render=True)
+    t.start()
 
 
 if __name__ == '__main__':
--- util/ports.py
+++ util/ports.py
@@ -7,6 +7,8 @@ def find_open_ports(num_ports):
         s = socket.socket()
         s.bind(('', 0))
         sockets.append(s)
-    # s.bind(('', 0))  # Bind to a free port provided by the host.
-
-    return [s.getsockname()[1] for s in sockets]  # Return the port number assigned.
+    ports = [s.getsockname()[1] for s in sockets]
+    # Close sockets to allow tensorflow distribute server to connect
+    for s in sockets:
+        s.close()
+    return ports
